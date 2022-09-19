d733a4c7dd35ddbf642616c18c551551ecab9e15
namu00 <skagns3571@naver.com>
2022-09-20 00:50:22 +0900

    Week03

---
+++ week03/diff.py
@@ -0,0 +1,8 @@
+#diff(1/(1+exp(-x))) == exp(-x)/(1+exp(-x))
+import math
+
+def diff(x):
+    return math.exp(-x)/(math.pow((1 + math.exp(-x)),2.0))
+
+input = int(input("입력: "))
+print("출력: ",diff(input))
---
+++ week03/palindrome.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+
+def verify(string):
+    rev = string[::-1]
+    if rev == string: return True
+    else: return False
+
+input = input("Key Word: ").replace(".","").replace(" ","")
+print("Result: ",verify(input))
