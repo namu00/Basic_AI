54e067b96724fd961bd838fbff75a33cac782082
skagns <skagns3571@naver.com>
2022-10-04 16:18:13 +0900

    Week 04

--- week04/csv_process.py
+++ week04/csv_process.py
@@ -1,23 +1,36 @@
 import csv
 import numpy
 
+def d_sum(list):
+    sum = 0
+    for d in list:
+        sum += int(d)
+    return sum
+
+def d_mean(list):
+    s = sum(list)
+    return s/len(list)
+
 f = open("week04/input.csv",'r')
 data = f.readlines()
 f.close()
 
-sum = []
-mean = []
+s = []
+mean_d = []
 
 for d in data[1:]:
     m = list(map(int,d.replace("\n","").split(",")[1:]))
-    sum.append(str(numpy.sum(m)))
-    mean.append(str(numpy.mean(m)))
+    #sum.append(str(numpy.sum(m)))
+    #mean.append(str(numpy.mean(m)))
+    s.append(str(d_sum(m)))
+    mean_d.append(str(d_mean(m)))
+
 
 d_write = data[1:]
 
 for i in range(len(d_write)):
     d_write[i] = d_write[i].replace("\n",",")
-    d_write[i] += sum[i] + "," + mean[i] + "\n"
+    d_write[i] += s[i] + "," + mean_d[i] + "\n"
 
 f = open("week04/output.csv",'w')
 f.write((data[0].replace("\n",",") + "합계,평균\n"))
--- week04/diff.py
+++ week04/diff.py
@@ -5,5 +5,5 @@ def g(x):
     if x <= 0: return 0
     else: return 1
 
-data = int(input("입력:"))
-print("출력: ")
+data = float(input("입력: "))
+print("출력: ",g(data))
